a70a65e5e88bd6b5a80592e53b960c828880ee8f
Varun Menon <varunmenon@uvic.ca>
2025-11-12 10:39:27 -0800

    Commit 1,Menu printing created in C file.

---
+++ Calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+  printf("\n Welcome to Simple Calculator\n");
+  printf("\nChoose one of the following options:");
+  printf("\n1. Addition");
+  printf("\n2. Subtract");
+  printf("\n3. Multiply");
+  printf("\n4. Divide");
+  printf("\n5. Modulus");
+  printf("\n6. Power");
+  printf("\n7. Exit");
+}
